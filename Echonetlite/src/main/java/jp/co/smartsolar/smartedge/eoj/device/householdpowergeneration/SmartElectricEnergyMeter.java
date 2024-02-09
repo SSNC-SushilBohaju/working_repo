ee890ab755c00e3be36a88d9eaeba6891cb8259f
SushilBohaju <112843183+SSNC-SushilBohaju@users.noreply.github.com>
2024-02-09 14:43:35 +0900

    Delete Echonetlite/src/main/java/jp/co/smartsolar/smartedge directory

--- Echonetlite/src/main/java/jp/co/smartsolar/smartedge/eoj/device/householdpowergeneration/SmartElectricEnergyMeter.java
+++
@@ -1,5 +0,0 @@
-package src.main.java.jp.co.smartsolar.smartedge.eoj.device.householdpowergeneration;
-
-public class SmartElectricEnergyMeter {
-    
-}
--- Echonetlite/src/main/java/jp/co/smartsolar/smartedge/exception/OutOfFrameException.java
+++
@@ -1,5 +0,0 @@
-package src.main.java.jp.co.smartsolar.smartedge.exception;
-
-public class OutOfFrameException extends Exception {
-
-}
